ed9601c6c863625485f1ac7233cedb0e73505243
Milton Vafana <146335315+nia-cloud-official@users.noreply.github.com>
2024-06-25 19:48:37 +0100

    Update login.php

--- login.php
+++ login.php
@@ -164,7 +164,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <body>
    <div class="login-form">
-      <img src="absorbed-base.surge.sh/logo.png" alt="Logo">
       <form method="post">
          <input type="text" name="email" placeholder="Email" required>
          <input type="password" name="password" placeholder="Password" required>
